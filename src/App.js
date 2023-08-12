afdea307d63a217346a1ee5640141577aeec07ee
Kanav Gupta <cancer.kanav70@gmail.com>
2023-08-12 15:32:30 +0530

    working with capturing params

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 
 import React from 'react';
 import ZerodhaConnect from './ZerodhaConnect';
+import RouterComponent from './components/router'
 import './styles/App.css';
+import {useSearchParams} from "react-router-dom"
+
 
 function App() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const val = searchParams.get("request_token");
+  console.log(val);
   return (
     <div className="container">
       <ZerodhaConnect />
+      <RouterComponent />
     </div>
   );
 }
